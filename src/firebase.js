4c92f699a14b03d6b5d38a9a49fe5775be4a61cd
Megh Patel <patelmeghmahesh2701@gmail.com>
2025-11-01 11:54:40 +0530

    refactor: update firebase configuration to use environment variables for improved security

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,16 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAlQBcrd1ZUlux5OkRLwgu-_fi1dGI3Y7s",
-  authDomain: "bakery-menu-cf3c6.firebaseapp.com",
-  projectId: "bakery-menu-cf3c6",
-  storageBucket: "bakery-menu-cf3c6.firebasestorage.app",
-  messagingSenderId: "796570263573",
-  appId: "1:796570263573:web:87ec77122d75372b60ee1c",
-  measurementId: "G-K70EX2BXE2",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
